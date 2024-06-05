149a40f4c55e41e54b4a269a8fae611f7a0fea44
Zuzanna Łysak <s30507@pjwstk.edu.pl>
2024-06-05 21:32:58 +0200

    fixed comments

--- untitled1/src/Main.java
+++ untitled1/src/Main.java
@@ -2,25 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        boolean continueWhile = true;
         StudentGroup sg = new StudentGroup();
         sg.nazwa = "grupa1";
 
-        while(true) {
+        while(continueWhile) {
             Scanner sc = new Scanner(System.in);
-            System.out.println("Podaj imię studenta:");
-            String fname = sc.next();
-            System.out.println("Podaj nazwisko studenta:");
-            String lname = sc.next();
-            System.out.println("Podaj email studenta:");
-            String email = sc.next();
 
             Student student = new Student();
-            student.fname = fname;
-            student.lname = lname;
-            student.email = email;
+            System.out.println("Podaj imię studenta:");
+            student.fname = sc.next();
+            System.out.println("Podaj nazwisko studenta:");
+            student.lname = sc.next();
+            System.out.println("Podaj email studenta:");
+            student.email = sc.next();
 
             sg.dodajStudenta(student);
+
+            System.out.println("Chcesz kontynuować, T jeśli tak, co kolwiek jeśli nie");
+            String decision = sc.next();
+
+            if(decision.equals("T") == false){
+                continueWhile = false;
+            }
         }
     }
 };
--- untitled1/src/StudentGroup.java
+++ untitled1/src/StudentGroup.java
@@ -1,40 +1,30 @@
+import java.util.ArrayList;
+
 public class StudentGroup {
 
     public StudentGroup(){
-        Student pustyStudent = new Student();
-        pustyStudent.fname = "";
-        pustyStudent.lname = "";
-        pustyStudent.email = "";
-        for (int i = 0; i < students.length; i++) {
-            students[i] = pustyStudent;
-        }
     }
     public String nazwa;
 
-    public Student[] students = new Student[15];
-
-    int index = 0;
+    public ArrayList<Student> students = new ArrayList<>();
 
     public void dodajStudenta(Student student) {
-        if (students.length > 15) {
+        if (students.size() > 15) {
             throw new IllegalStateException("Maksymalna liczba osób w grupie to 15");
         }
         if (czyIstnieje(student)) {
             throw new IllegalStateException("Student został już dodany");
         }
 
-        students[index] = student;
-        index++;
+        students.add(student);
     }
     public Boolean czyIstnieje(Student student) {
-        Boolean istnieje = false;
-
-        for (int i = 0; i < students.length; i++) {
-                if (student.email.equals(students[i].email)) {
-                    istnieje = true;
+        for (int i = 0; i < students.size(); i++) {
+                if (student.email.equals(students.get(i).email)) {
+                    return true;
                 }
         }
 
-        return istnieje;
+        return false;
     }
 }
